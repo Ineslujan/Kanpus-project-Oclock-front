c95666f9287b8cd55ad23d07972757a52decd9a4
LudovicLecoq <ludovic.react@gmail.com>
2022-03-30 17:49:54 +0200

    create connexion component (mobile css) wip

--- src/components/Connexion/Connexion.js
+++ src/components/Connexion/Connexion.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import ConnexionForm from '../ConnexionForm/ConnexionForm';
 
-import './connexion.css'
+
+import './connexion.css';
 
 export default function Connexion() {
+
+
+ 
   return (
     <>
         <Navbar />
         <div className="connexion-container">
-            <section className="connexion-container-left">
-                </section>
+            <section className="connexion-container-left"></section>
             <section className="connexion-container-right">
-
+              <ConnexionForm />
             </section>
         </div>
         <Footer />
---
+++ src/components/ConnexionForm/ConnexionForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import './connexionForm.css'
+
+export default function ConnexionForm() {
+    const {register, handleSubmit, formState: { errors }, reset, watch} = useForm({});
+
+    const onSubmit = data =>  {
+        console.log(data);
+    }
+
+
+  return (
+    <form className="connexion-form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="connexion-form-container">
+            <label htmlFor="connexion-email" className="connexion-form-label">Email :</label> <br/>
+            <input type="text" className="connexion-form-input" {...register("email", { required: true })} /> <br/>
+            {errors.email && <span>Vous devez rentrer un email pour vous connecter</span>}
+        </div>
+        <div className="connexion-form-container">
+            <label htmlFor="connexion-password" className="connexion-form-label">Mot de passe :</label> <br/>
+            <input type="text" className="connexion-form-inputl" {...register("password", { required: true })} /> <br/>
+            {errors.password && <span>Vous devez rentrer votre mot de passe pour vous connecter</span>}
+        </div>
+        <button className="connexion-form-button">Valider</button>
+    </form>
+  )
+}
