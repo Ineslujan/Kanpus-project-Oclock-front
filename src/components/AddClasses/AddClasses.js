1ea55de68acabb3a7577a27dcebc90a2894471a2
LudovicLecoq <ludovic.react@gmail.com>
2022-04-04 15:55:52 +0200

    create addClasses component wip (refact)

--- src/components/AddClasses/AddClasses.js
+++ src/components/AddClasses/AddClasses.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import PickDate from '../DatePicker/PickDate';
-import TimePicker from '../TimePicker/TimePicker';
-import { DateTime } from "luxon";
+
+import AddClassesFormPart1 from '../AddClassesFormPart1/AddClassesFormPart1';
 
 import AddClassesMenu from '../AddClassesMenu/AddClassesMenu';
 import AddPlaceForm from '../AddPlaceForm/AddPlaceForm';
@@ -10,31 +8,13 @@ import AddTeachersForm from '../AddTeachersForm/AddTeachersForm';
 import AddStudentsForm from '../AddStudentsForm/AddStudentsForm';
 import AddTextForm from '../../container/AddTextForm/AddTextForm';
 
-import { requestEvent } from '../../requests/AddClassesFormRequest';
-
 import './addClasses.scss'
 
 export default function AddClasses() {
 
-    const {register, handleSubmit, formState: { errors }, reset, watch} = useForm({});
-    const watchName = watch("name");
-
     const [allDatasForm, setAllDatasForm] = useState({})
 
     const [closeFormPart1, setCloseFormPart1] = useState(false);
-    
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
-
-    const [startTime, setStartTime] = useState('9:00');
-    const [endTime, setEndTime] = useState('17:30');
-    const [showStartTimePicker, setShowStartTimePicker] = useState(false)
-    const [showEndTimePicker, setShowEndTimePicker] = useState(false)
-    
-    let newStartDate = DateTime.fromJSDate(startDate);
-    let newEndDate = DateTime.fromJSDate(endDate);
-
-    const [valideButton, setValidateButton] = useState(false);
     const [closeFormPart2, setCloseFormPart2] = useState(true);
 
     const [tabClasseRoom, setTabClasseRoom]= useState(null);
@@ -53,14 +33,6 @@ export default function AddClasses() {
     const [validAllFormButton, setValidAllFormButton] = useState(false);
 
     useEffect(() => {
-        if(!newStartDate.day || !newEndDate.day || !watchName ){
-            setValidateButton(false);
-        } else {
-            setValidateButton(true);
-        }       
-    }, [newStartDate.day, newEndDate.day, watchName ]);
-
-    useEffect(() => {
         if(!closeFormPart1){
             setCloseFormPart2(true)
         } else {
@@ -75,39 +47,12 @@ export default function AddClasses() {
             setValidAllFormButton(true);
         }
     }, [tabTeachersAdded, classeRoom]);
-
-  
     
-    
-    const onSubmit = data =>  {
-        data.start_date = `${newStartDate.year}-${newStartDate.month}-${newStartDate.day} ${startTime}:00 ${newStartDate.offsetNameShort}`;
-        data.end_date = `${newEndDate.year}-${newEndDate.month}-${newEndDate.day} ${endTime}:00 ${newEndDate.offsetNameShort}`;
-        
-        setAllDatasForm({
-            name: data.name,
-            start_date: data.start_date,
-            end_date: data.start_date,
-        })
-        const getDatas = async () => {
-            const datas = await requestEvent({
-                name: data.name,
-                start_date: data.start_date,
-                end_date: data.start_date,
-            });
-            if(datas.status === 200){
-                setTabTeachers(datas.data.former);
-                setTabClasseRoom(datas.data.place);
-                setCloseFormPart1(true);
-            }
-        } 
-        getDatas();
-    }
-
     const submitForm = () => {
         console.log("submit form =>")
         const tabTrainee= [];
         tabSelectedStudents.forEach(item=> {tabTrainee.push(item.id)});
-        
+
         setAllDatasForm({
             ...allDatasForm,
             place_id: classeRoom, // on envoie les id
@@ -121,35 +66,9 @@ export default function AddClasses() {
         console.log(allDatasForm);
     }
 
-
   return (
-
     <div className="create-classes-container">
-        {closeFormPart1 ? 
-            <div className="data-open-container">
-                <button className="date-open" onClick={()=> setCloseFormPart1(false)}>{watchName}</button>
-                <button className="date-open" onClick={()=> setCloseFormPart1(false)}>{`${newStartDate.weekdayLong} ${newStartDate.day}  ${newStartDate.monthLong} ${newStartDate.year} ${startTime}`}</button>
-                <button className="date-open" onClick={()=> setCloseFormPart1(false)}>{`${newEndDate.weekdayLong} ${newEndDate.day} ${newEndDate.monthLong} ${newEndDate.year} ${endTime}`}</button>
-            </div>
-        :
-            <form className="create-date-form" onSubmit={handleSubmit(onSubmit)}>
-                <div className="create-date-form-container">
-                    <label htmlFor="create-date-name" className="create-date-name-label">nom du cours</label>
-                    <input type="text" className="create-date-form-input" {...register("name", { required: true })} />
-                    {errors.email && <span>Vous devez rentrer un nom pour ce cours</span>}
-                </div>
-
-                <label htmlFor="create-date-name" className="create-date-name-label">choisissez une date</label>
-                <PickDate startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} />
-                <label htmlFor="create-date-name" className="create-date-name-label">horaires</label>
-
-                <div className="create-date-form-time-container">
-                    {showStartTimePicker ? <TimePicker time={startTime} setTime={setStartTime} setShowPicker={setShowStartTimePicker} /> : <button className="date-time-button" onClick={()=> setShowStartTimePicker(true)}>{startTime}</button>}
-                    {showEndTimePicker ? <TimePicker  time={endTime} setTime={setEndTime} setShowPicker={setShowEndTimePicker} /> : <button className="date-time-button"  onClick={()=> setShowEndTimePicker(true)}>{endTime}</button>}
-                </div>
-                {valideButton ? <button className="date-form-button">Valider</button> : <button className="date-form-button" disabled>Valider</button>}
-            </form>
-        }
+        <AddClassesFormPart1 setAllDatasForm={setAllDatasForm} setTabTeachers={setTabTeachers} setTabClasseRoom={setTabClasseRoom} closeFormPart1={closeFormPart1} setCloseFormPart1={setCloseFormPart1} />
         {!closeFormPart2 && 
             <div className="container-form-part2">
                 <AddClassesMenu tabSelectedStudents={tabSelectedStudents} setTabSelectedStudents={setTabSelectedStudents} />
--- src/components/AddClassesFormPart1/AddClassesFormPart1.js
+++ src/components/AddClassesFormPart1/AddClassesFormPart1.js
@@ -1,114 +1,88 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import { useForm } from 'react-hook-form';
 import PickDate from '../DatePicker/PickDate';
 import TimePicker from '../TimePicker/TimePicker';
 import { DateTime } from "luxon";
 
-export default function AddClassesFormPart1( { setNewStartDate, setNewEndDate, endTime, setEndTime, startTime, setStartTime, onSubmit} ) {
+import { requestEvent } from '../../requests/AddClassesFormRequest';
+
+export default function AddClassesFormPart1({ setAllDatasForm, setTabTeachers, setTabClasseRoom, setCloseFormPart1, closeFormPart1}) {
+
     const {register, handleSubmit, formState: { errors }, reset, watch} = useForm({});
     const watchName = watch("name");
-
-    const [valideButton, setValidateButton] = useState(false);
-  
 
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
 
-    let validStartDate = DateTime.fromJSDate(startDate);
-    let validEndDate = DateTime.fromJSDate(endDate);
+    const [startTime, setStartTime] = useState('9:00');
+    const [endTime, setEndTime] = useState('17:30');
+    const [showStartTimePicker, setShowStartTimePicker] = useState(false);
+    const [showEndTimePicker, setShowEndTimePicker] = useState(false);
 
-    const [showStartTimePicker, setShowStartTimePicker]= useState(false)
-    const [showEndTimePicker, setShowEndTimePicker]= useState(false)
-
+    const [valideButton, setValidateButton] = useState(false);
+    
+    let newStartDate = DateTime.fromJSDate(startDate);
+    let newEndDate = DateTime.fromJSDate(endDate);
 
     useEffect(() => {
-        if(!validStartDate.day || !validEndDate.day ||!watchName ){
+        if(!newStartDate.day || !newEndDate.day || !watchName ){
             setValidateButton(false);
         } else {
             setValidateButton(true);
-            setNewStartDate(validStartDate);
-            setNewEndDate(validEndDate);
         }       
-    }, [validStartDate.day, validEndDate.day , watchName ]);
+    }, [newStartDate.day, newEndDate.day, watchName ]);
 
-  return (
-    <form className="create-date-form" onSubmit={handleSubmit(onSubmit)}>
-    <div className="create-date-form-container">
-        <label htmlFor="create-date-name" className="create-date-name-label">nom du cours</label>
-        <input type="text" className="create-date-form-input" {...register("name", { required: true })} />
-        {errors.email && <span>Vous devez rentrer un nom pour ce cours</span>}
-    </div>
+    const onSubmit = data =>  {
+        data.start_date = `${newStartDate.year}-${newStartDate.month}-${newStartDate.day} ${startTime}:00 ${newStartDate.offsetNameShort}`;
+        data.end_date = `${newEndDate.year}-${newEndDate.month}-${newEndDate.day} ${endTime}:00 ${newEndDate.offsetNameShort}`;
+        
+        setAllDatasForm({
+            name: data.name,
+            start_date: data.start_date,
+            end_date: data.start_date,
+        })
+        const getDatas = async () => {
+            const datas = await requestEvent({
+                name: data.name,
+                start_date: data.start_date,
+                end_date: data.start_date,
+            });
+            if(datas.status === 200){
+                setTabTeachers(datas.data.former);
+                setTabClasseRoom(datas.data.place);
+                setCloseFormPart1(true);
+            }
+        } 
+        getDatas();
+    }
 
-    <label htmlFor="create-date-name" className="create-date-name-label">choisissez une date</label>
-    <PickDate startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} />
-    <label htmlFor="create-date-name" className="create-date-name-label">horaires</label>
+    return (
+        <>
+            {closeFormPart1 ? 
+                <div className="data-open-container">
+                    <button className="date-open" onClick={()=> setCloseFormPart1(false)}>{watchName}</button>
+                    <button className="date-open" onClick={()=> setCloseFormPart1(false)}>{`${newStartDate.weekdayLong} ${newStartDate.day}  ${newStartDate.monthLong} ${newStartDate.year} ${startTime}`}</button>
+                    <button className="date-open" onClick={()=> setCloseFormPart1(false)}>{`${newEndDate.weekdayLong} ${newEndDate.day} ${newEndDate.monthLong} ${newEndDate.year} ${endTime}`}</button>
+                </div>
+            :
+                <form className="create-date-form" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="create-date-form-container">
+                        <label htmlFor="create-date-name" className="create-date-name-label">nom du cours</label>
+                        <input type="text" className="create-date-form-input" {...register("name", { required: true })} />
+                        {errors.email && <span>Vous devez rentrer un nom pour ce cours</span>}
+                    </div>
 
-    <div className="create-date-form-time-container">
-        {showStartTimePicker ? <TimePicker time={startTime} setTime={setStartTime} setShowPicker={setShowStartTimePicker} /> : <button className="date-time-button" onClick={()=> setShowStartTimePicker(true)}>{startTime}</button>}
-        {showEndTimePicker ? <TimePicker  time={endTime} setTime={setEndTime} setShowPicker={setShowEndTimePicker} /> : <button className="date-time-button"  onClick={()=> setShowEndTimePicker(true)}>{endTime}</button>}
-    </div>
-    {valideButton ? <button className="date-form-button" >Valider</button> : <button className="date-form-button" disabled>Valider</button>}
+                    <label htmlFor="create-date-name" className="create-date-name-label">choisissez une date</label>
+                    <PickDate startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} />
+                    <label htmlFor="create-date-name" className="create-date-name-label">horaires</label>
 
-</form>
-  )
+                    <div className="create-date-form-time-container">
+                        {showStartTimePicker ? <TimePicker time={startTime} setTime={setStartTime} setShowPicker={setShowStartTimePicker} /> : <button className="date-time-button" onClick={()=> setShowStartTimePicker(true)}>{startTime}</button>}
+                        {showEndTimePicker ? <TimePicker  time={endTime} setTime={setEndTime} setShowPicker={setShowEndTimePicker} /> : <button className="date-time-button"  onClick={()=> setShowEndTimePicker(true)}>{endTime}</button>}
+                    </div>
+                    {valideButton ? <button className="date-form-button">Valider</button> : <button className="date-form-button" disabled>Valider</button>}
+                </form>
+            }
+        </>
+    )
 }
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import AddClassesFormPart1 from '../AddClassesFormPart1/AddClassesFormPart1';
-
-// import './addClasses.scss'
-
-// export default function AddClasses() {
-
-//     const [closeFormPart1, setCloseFormPart1] = useState(false);
-
-
-//     const [startTime, setStartTime] = useState('9:00');
-//     const [endTime, setEndTime] = useState('13:30');
-    
-//     // let newStartDate = DateTime.fromJSDate(startDate);
-//     // let newEndDate = DateTime.fromJSDate(endDate);
-
-//     const [newStartDate, setNewStartDate] = useState();
-//     const [newEndDate, setNewEndDate] = useState()
-
-
-//     const [formPart2, setFormPart2] = useState(false)
-
-
-
-//     const onSubmit = data =>  {
-//         data.start_date = `${newStartDate.year}-${newStartDate.month}-${newStartDate.day} ${startTime}:00 ${newStartDate.offsetNameShort}`;
-//         data.end_date = `${newEndDate.year}-${newEndDate.month}-${newEndDate.day} ${endTime}:00 ${newEndDate.offsetNameShort}`;
-//         setCloseFormPart1(true);
-//         console.log(data);
-//         console.log(newStartDate);
-//     }
-
-//   return (
-
-//     <div className="create-classes-container">
-//         {closeFormPart1 ? 
-//             <div className="data-open-container">
-//                 <button className="date-open" onClick={()=> setCloseFormPart1(false)}>{`${newStartDate.weekdayShort} ${newStartDate.day}  ${newStartDate.monthShort} ${newStartDate.year} ${startTime}`}</button>
-//                 <button className="date-open" onClick={()=> setCloseFormPart1(false)}>{`${newEndDate.weekdayShort} ${newEndDate.day} ${newEndDate.monthShort} ${newEndDate.year} ${endTime}`}</button>
-//             </div>
-//         :
-//         <AddClassesFormPart1 
-//             onSubmit={onSubmit}  
-//             startTime={startTime} 
-//             setStartTime={setStartTime} 
-//             endTime={endTime} 
-//             setEndTime={setEndTime} 
-//             setNewStartDate={setNewStartDate} 
-//             setNewEndDate={setNewEndDate} 
-//         />
-//         }
-
-//     </div>
-//   )
-// }
