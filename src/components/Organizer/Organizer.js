8add61f380f84c535d360cb19312a99a1b8d7486
Melusyne <theinesalvarez.pro@gmail.com>
2022-04-14 14:05:29 +0200

    Add modal's opening on events

--- src/components/Organizer/Organizer.js
+++ src/components/Organizer/Organizer.js
@@ -6,6 +6,8 @@ import { ajustX, defineCardWidth } from './cards/cardPlacement';
 import useWindowDimensions from '../../customHooks/getWindowDimensions';
 import GridLayout from "react-grid-layout";
 import { v4 as uuid } from 'uuid';
+import MycourseModal from '../../components/MyCourseModal/MyCourseModal'
+
 
 import '../../../node_modules/react-grid-layout/css/styles.css'
 import '../../../node_modules/react-resizable/css/styles.css'
@@ -23,14 +25,20 @@ export default function Organizer() {
     const [previousWeek, setPreviousWeek] = useState(DateTime.fromJSDate(new Date(firstDayOfWeek)).minus({ days: 7 }));
     const [places, setPlaces] = useState([]);
     const [events, setEvents] = useState([]);
-    console.log(events);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [eventModal, setEventModal] = useState({});
 
-    const changeToPreviousWeek= () => {
+    const openModal = (event) => {
+        setModalIsOpen(!modalIsOpen);
+        setEventModal(event)
+    }
+
+    const changeToPreviousWeek = () => {
         console.log("-----changement de semaine-----");
         setFirstDayOfWeek(firstDayOfWeek => DateTime.fromJSDate(new Date(firstDayOfWeek)).minus({ days: 7 }));
     };
 
-    const changeToNextWeek= () => {
+    const changeToNextWeek = () => {
         console.log("-----changement de semaine-----");
         setFirstDayOfWeek(firstDayOfWeek => DateTime.fromJSDate(new Date(firstDayOfWeek)).plus({ days: 7 }));
     };
@@ -41,51 +49,36 @@ export default function Organizer() {
 
 
 
-
-
-
-
-
-    /**
-     * WEEKEND
-     * décomposer les jours de l'evenement
-     * Si samedi (6) et dimanche (7) traversé entièrement => soustraire 2 * 2
-     * Si  commence un samedi ou dimanche : supprimer directement le ou les jour entier
-     * Recomposer la carte avant le weekend
-     * Recomposer la carte après le weekend
-     * */
     const checkWeekend = (dataEvents, date) => {
         const dataReal = dataEvents.map(event => {
-            return {...event}
+            return { ...event }
         });
         const firstWeekDay = date;
-        
-        
+
+
         dataReal.forEach((d) => {
-            const objectStartDate = DateTime.fromISO(d.start_date).toObject();
-            const objectEndDate = DateTime.fromISO(d.end_date).toObject();
             const startDateEventDay = DateTime.fromISO(d.start_date)
             const endDateEventDay = DateTime.fromISO(d.end_date)
             const startDateEventWeekday = DateTime.fromISO(d.start_date).toFormat('c')
             const endDateEventWeekday = DateTime.fromISO(d.end_date).toFormat('c')
-            
+
             const diffEvent = endDateEventDay.diff(startDateEventDay, 'weeks')
             const gapWeek = diffEvent.weeks
-            
+
             if (gapWeek >= 1) {
-                
+                //! A mettre en place
             }
             else if (startDateEventWeekday > endDateEventWeekday) {
                 if (startDateEventDay > firstWeekDay) {
 
                     const toAddInEndDate = Math.abs(Number(startDateEventWeekday) - 5);
-                    const newDate = startDateEventDay.plus({days : toAddInEndDate}).toFormat("yyyy-MM-dd")
+                    const newDate = startDateEventDay.plus({ days: toAddInEndDate }).toFormat("yyyy-MM-dd")
                     d.end_date = `${newDate}T22:00:00.000Z`;
                 }
                 if (startDateEventDay < firstWeekDay) {
 
                     const toRemoveInStartDate = Math.abs(Number(endDateEventWeekday) - 1);
-                    const newDate = endDateEventDay.minus({days : toRemoveInStartDate}).toFormat("yyyy-MM-dd")
+                    const newDate = endDateEventDay.minus({ days: toRemoveInStartDate }).toFormat("yyyy-MM-dd")
                     d.start_date = `${newDate}T03:00:00.000Z`;
                 }
             }
@@ -137,9 +130,9 @@ export default function Organizer() {
         // adjustX returns 0 for before noon and 1 after noon
         const isAfternoon = ajustX(eventHour);
 
-        for(let day = 1,  shift = 0; day <= 5; day++, shift++) {
-            if(dayNumber === day) {
-                return (shift+day) + isAfternoon;
+        for (let day = 1, shift = 0; day <= 5; day++, shift++) {
+            if (dayNumber === day) {
+                return (shift + day) + isAfternoon;
             }
         }
         return 1;
@@ -153,92 +146,100 @@ export default function Organizer() {
     }
 
     return (
+        <>
+            <GridLayout className="layout" cols={11} onDragStop={onDragStopTest} onResizeStop={onResizeStopTest} rowHeight={60} compactType={null} preventCollision={true} rows={8} width={windowWidth} maxRows={places.length + 1} >
 
-        <GridLayout className="layout" cols={11} onDragStop={onDragStopTest} onResizeStop={onResizeStopTest} rowHeight={60} compactType={null} preventCollision={true} rows={8} width={windowWidth} maxRows={places.length + 1} >
-
-            <div data-grid={{ x: 0, y: 0, w: 1, h: 1, static: true }} data-organizer-type="blank" key={uuid()} >
-                <div className="blank-row">
-                    <div className="year">{firstDayOfWeek.year}</div>
-                    <div>
-                        <button className="weeks-button-left" onClick={changeToPreviousWeek}>&lt;</button>
-                        <button className="weeks-button-right" onClick={changeToNextWeek}>&gt;</button>
-                    </div>
-                </div>
-                <div className="row"></div>
-            </div>
-
-            <div data-grid={{ x: 1, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 0 }).weekdayLong} {firstDayOfWeek.plus({ days: 0 }).day} {firstDayOfWeek.plus({ days: 0 }).monthLong}
-                <div className="column"></div> {/** For drawing externals borders in CSS */}
-                <div className="column-middle"></div> {/** For drawing middle borders in CSS */}
-            </div>
-            <div data-grid={{ x: 3, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 1 }).weekdayLong} {firstDayOfWeek.plus({ days: 1 }).day} {firstDayOfWeek.plus({ days: 1 }).monthLong}
-                <div className="column"></div>
-                <div className="column-middle"></div>
-            </div>
-            <div data-grid={{ x: 5, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 2 }).weekdayLong} {firstDayOfWeek.plus({ days: 2 }).day} {firstDayOfWeek.plus({ days: 2 }).monthLong}
-                <div className="column"></div>
-                <div className="column-middle"></div>
-            </div>
-            <div data-grid={{ x: 7, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 3 }).weekdayLong} {firstDayOfWeek.plus({ days: 3 }).day} {firstDayOfWeek.plus({ days: 3 }).monthLong}
-                <div className="column"></div>
-                <div className="column-middle"></div>
-            </div>
-            <div data-grid={{ x: 9, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 4 }).weekdayLong} {firstDayOfWeek.plus({ days: 4 }).day} {firstDayOfWeek.plus({ days: 4 }).monthLong}
-                <div className="column"></div>
-                <div className="column-middle"></div>
-            </div>
-
-
-            {
-                places.map((item) => {
-                    return (
-                        <div data-grid={{ x: 0, y: (item.position + 1), w: 1, h: 1, static: true }} data-organizer-type="row" key={uuid()}>
-                            {item.name}
-                            <div className="row"></div>
+                <div data-grid={{ x: 0, y: 0, w: 1, h: 1, static: true }} data-organizer-type="blank" key={uuid()} >
+                    <div className="blank-row">
+                        <div className="year">{firstDayOfWeek.year}</div>
+                        <div>
+                            <button className="weeks-button-left" onClick={changeToPreviousWeek}>&lt;</button>
+                            <button className="weeks-button-right" onClick={changeToNextWeek}>&gt;</button>
                         </div>
-                    )
-                })
-            }
-            {
-                places.length > 0 && events.length > 0 && events.map((event) => {
+                    </div>
+                    <div className="row"></div>
+                </div>
 
-                    console.log(event);
-                    const startDayNumber = Number(UTCDate(event.start_date, "E"));
-                    const startDayHour = UTCDate(event.start_date, "HH:mm");
-                    const endDayHour = UTCDate(event.end_date, "HH:mm");
-                    const formatedStartDate = UTCDate(event.start_date, "yyyy-MM-dd");
-                    const formatedEndDate = UTCDate(event.end_date, "yyyy-MM-dd");
+                <div data-grid={{ x: 1, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 0 }).weekdayLong} {firstDayOfWeek.plus({ days: 0 }).day} {firstDayOfWeek.plus({ days: 0 }).monthLong}
+                    <div className="column"></div> {/** For drawing externals borders in CSS */}
+                    <div className="column-middle"></div> {/** For drawing middle borders in CSS */}
+                </div>
+                <div data-grid={{ x: 3, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 1 }).weekdayLong} {firstDayOfWeek.plus({ days: 1 }).day} {firstDayOfWeek.plus({ days: 1 }).monthLong}
+                    <div className="column"></div>
+                    <div className="column-middle"></div>
+                </div>
+                <div data-grid={{ x: 5, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 2 }).weekdayLong} {firstDayOfWeek.plus({ days: 2 }).day} {firstDayOfWeek.plus({ days: 2 }).monthLong}
+                    <div className="column"></div>
+                    <div className="column-middle"></div>
+                </div>
+                <div data-grid={{ x: 7, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 3 }).weekdayLong} {firstDayOfWeek.plus({ days: 3 }).day} {firstDayOfWeek.plus({ days: 3 }).monthLong}
+                    <div className="column"></div>
+                    <div className="column-middle"></div>
+                </div>
+                <div data-grid={{ x: 9, y: 0, w: 2, h: 1, static: true }} data-organizer-type="column" key={uuid()}> {firstDayOfWeek.plus({ days: 4 }).weekdayLong} {firstDayOfWeek.plus({ days: 4 }).day} {firstDayOfWeek.plus({ days: 4 }).monthLong}
+                    <div className="column"></div>
+                    <div className="column-middle"></div>
+                </div>
 
-                    const verticalEventPos = Number(places[event.place_position].position + 1);
-                    
-                    const x = calculateXPos(startDayNumber, startDayHour);
-                    const w = defineCardWidth(formatedStartDate, startDayHour, formatedEndDate, endDayHour);
-                    event.event_id === 17 && console.log("X =>",x,"W =>",w)
 
-                    return (
-                        <div data-grid={{
-                            x,
-                            y: verticalEventPos,
-                            w,
-                            h: 1,
-                            maxH: 1
+                {
+                    places.map((item) => {
+                        return (
+                            <div data-grid={{ x: 0, y: (item.position + 1), w: 1, h: 1, static: true }} data-organizer-type="row" key={uuid()}>
+                                {item.name}
+                                <div className="row"></div>
+                            </div>
+                        )
+                    })
+                }
+
+                {
+                    places.length > 0 && events.length > 0 && events.map((event) => {
+
+                        const startDayNumber = Number(UTCDate(event.start_date, "E"));
+                        const startDayHour = UTCDate(event.start_date, "HH:mm");
+                        const endDayHour = UTCDate(event.end_date, "HH:mm");
+                        const formatedStartDate = UTCDate(event.start_date, "yyyy-MM-dd");
+                        const formatedEndDate = UTCDate(event.end_date, "yyyy-MM-dd");
+
+                        const verticalEventPos = Number(places[event.place_position].position + 1);
+
+                        const x = calculateXPos(startDayNumber, startDayHour);
+                        const w = defineCardWidth(formatedStartDate, startDayHour, formatedEndDate, endDayHour);
+
+                        return (
+
+                            <div data-grid={{
+                                x,
+                                y: verticalEventPos,
+                                w,
+                                h: 1,
+                                maxH: 1,
+                                static: true
                             }}
-                            data-organizer-type="card"
-                            style={{ backgroundColor: event.former[0].color }}
-                            key={uuid()}
-                        >
-                            <div className="card-content">
-                                {event.name}
-                                <div className="card-content-time">
-                                    <span>{startDayHour}</span>
-                                    <span>{endDayHour}</span>
+                                data-organizer-type="card"
+                                style={{ backgroundColor: event.former[0].color }}
+                                key={uuid()}
+                                onClick={() => openModal(event)}
+                            >
+                                <div className="card-content">
+                                    {event.name}
+                                    <div className="card-content-time">
+                                        <span>{startDayHour}</span>
+                                        <span>{endDayHour}</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
 
-        </GridLayout>
+                        )
+                    })
+
+                }
+
+            </GridLayout>
+            {modalIsOpen &&
+                <MycourseModal modalIsOpen={modalIsOpen} openModal={openModal} datas={eventModal} />}
+
+        </>
     )
 }
