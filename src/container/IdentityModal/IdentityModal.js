149587e34c11462d734549154cd069c8c7d53859
deldon <r.deldon@gmail.com>
2022-04-20 00:05:20 +0200

    modifying css of identityModal

--- src/container/IdentityModal/IdentityModal.js
+++ src/container/IdentityModal/IdentityModal.js
@@ -13,7 +13,7 @@ import './identityModal.scss'
 import UserForm from '../UserForm/UserForm';
 
 
-export default function IdentityModal({item, modalIsOpen, closeIdentityModal, setSelectedPromo, setAllPromo, setSeeUpdateModal, getStudents}) {
+export default function IdentityModal({ item, modalIsOpen, closeIdentityModal, setSelectedPromo, setAllPromo, setSeeUpdateModal, getStudents }) {
     const { authentication, setAuthentication } = useContext(AuthenticationContext);
 
     Modal.setAppElement(document.getElementById('root'));
@@ -23,7 +23,7 @@ export default function IdentityModal({item, modalIsOpen, closeIdentityModal, se
 
     const setUpdate = () => {
         console.log("heyhi")
-        setUpdateModal(x=>!x);
+        setUpdateModal(x => !x);
         console.log(updateModal)
     }
 
@@ -33,17 +33,17 @@ export default function IdentityModal({item, modalIsOpen, closeIdentityModal, se
 
     const deleteStudent = async (id) => {
         const deleteOneStudent = await deleteUser(id, authentication.token);
-            if(deleteOneStudent.status===200){
-                // console.log("id",id);
-                const getStudents = async () => {
-                    const trainees = await requestTrainee(authentication.token);
+        if (deleteOneStudent.status === 200) {
+            // console.log("id",id);
+            const getStudents = async () => {
+                const trainees = await requestTrainee(authentication.token);
+                // console.log('trainee=>',trainees.data)
+                if (trainees.status === 200) {
+                    setAllPromo(trainees.data);
+                    setSelectedPromo(trainees.data[0]);
                     // console.log('trainee=>',trainees.data)
-                    if(trainees.status === 200){
-                        setAllPromo(trainees.data);
-                        setSelectedPromo(trainees.data[0]);
-                        // console.log('trainee=>',trainees.data)
-                    }
                 }
+            }
             getStudents();
             setSeeConfirmationModal(modal => !modal);
             closeIdentityModal();
@@ -55,68 +55,88 @@ export default function IdentityModal({item, modalIsOpen, closeIdentityModal, se
         setSeePasswordModal(modal => !modal)
     }
 
-  return (
-    <div>
-        <Modal
-            isOpen={modalIsOpen}
-            className='Modal'
-            overlayClassName='Overlay'
-        >
-            <div className="modal-button-close">
-                <button className="close" onClick={closeIdentityModal}><img src={svgCircle} alt="close-icon" /></button>
-            </div>
-            <div className="identity-modal-container">
-                <div className="modal-icones">
-        
-                    <button className="modal-icone" onClick={setUpdate}><img src={Pen} alt="pen"/></button> 
+    return (
+        <div>
+            <Modal
+                isOpen={modalIsOpen}
+                className='Modal'
+                overlayClassName='Overlay'
+            >
+                <div className="modal-button-close">
+                    <button className="close" onClick={closeIdentityModal}><img src={svgCircle} alt="close-icon" /></button>
+                </div>
+                <div className="identity-modal-warpper">
+                    <div className="identity-modal-right">
+                        <div className="identity-modal-main">
 
-                    {updateModal && <UserForm data={item} updateModal={updateModal} setUpdate={setUpdate} setSeeUpdateModal={setSeeUpdateModal} getStudents={getStudents} closeIdentityModal={closeIdentityModal}   /> }
 
-                    <button className="modal-icone" onClick={confirmationModal}><img src={Trash} alt="trash" /></button>
+                            <img className="picture" src={item.image} alt="student picture" />
 
-                    <Modal isOpen={seeConfirmationModal} >
-                        <div className="modal-button-close">
-                            <div className="modal-confirmation-delete">
-                                <button className="close" onClick={confirmationModal}>x</button>
+                            <div className="identity-modal-container-name">
+                                <span className="identity-modal-name">{item.firstname} {item.lastname}</span>
+
+                                <p className="identity-modal-promo">{item.promo}</p>
                             </div>
                         </div>
-                        <div className="modal-confirmation-delete-button">
-                            <div className="modal-confirmation-title">
-                                <p>Voulez-vous vraiment supprimer cet élève ?</p>
+                    </div>
+                    <div className="identity-modal-left">
+                        <div className="identity-modal-container">
+
+                            <div className="identity-modal-container-adress">
+                                <p className="identity-modal-container-title">Adress :</p>
+                                <p className="identity-modal-container-content"> {item.address} </p>
                             </div>
-                            <div className="modal-confirmation-delete-response">
-                                <button className="modal-confirmation-response" onClick={confirmationModal}>Non</button>
-                                <button className="modal-confirmation-response" onClick={()=> deleteStudent(item.id) }>Oui</button>
+                            <div className="identity-modal-container-phone">
+                                <p className="identity-modal-container-title">Téléphone :</p>
+                                <p className="identity-modal-container-content"> {item.phone_number} </p>
                             </div>
+                            <div className="identity-modal-container-mail">
+                                <p className="identity-modal-container-title">Email :</p>
+                                <p className="identity-modal-container-content"> {item.email} </p>
+                            </div>
+
+                            <ModalPassword passwordModal={passwordModal} seePasswordModal={seePasswordModal} />
+
                         </div>
-                    </Modal>
 
-                </div>
+                        <div className="identity-modal-footer">
 
-                <div className="identity-modal-container-name">
-                    <span className="identity-modal-name">{item.firstname}</span> <span className="identity-modal-name">{item.lastname}</span>
-                </div>
-                <img className="picture" src={item.image} alt="student picture" />
-                <p className="identity-modal-promo">{item.promo}</p>
-                <div className="identity-modal-container-adress">
-                    <p className="adress-title">Adress :</p>
-                    <p className="adress-content"> {item.address} </p>
-                </div>
-                <div className="identity-modal-container-phone">
-                    <p className="phone-title">Téléphone :</p>
-                    <p className="phone-content"> {item.phone_number} </p>
-                </div>
-                <div className="identity-modal-container-mail">
-                    <p className="mail-title">Email :</p>
-                    <p className="mail-content"> {item.email} </p>
-                </div>
-                <button className="button-modal" onClick={passwordModal}>Changez le mot de passe</button>
-                <ModalPassword passwordModal={passwordModal} seePasswordModal={seePasswordModal} />
-           
-            </div>
+                            <button className="identity-modal-icone" onClick={setUpdate}><img src={Pen} alt="pen" /></button>
 
-        </Modal>
-        {console.log(item)}
-    </div>
-  )
+                            {updateModal && <UserForm data={item} updateModal={updateModal} setUpdate={setUpdate} setSeeUpdateModal={setSeeUpdateModal} getStudents={getStudents} closeIdentityModal={closeIdentityModal} />}
+
+                            <button className="identity-modal-icone" onClick={confirmationModal}><img src={Trash} alt="trash" /></button>
+
+                            <Modal
+                                isOpen={seeConfirmationModal}
+                                className='Modal'
+                                overlayClassName='Overlay'
+                            >
+                                <div className="modal-button-close">
+                                    <div className="modal-confirmation-delete">
+                                        <button className="close" onClick={confirmationModal}>x</button>
+                                    </div>
+                                </div>
+                                <div className="modal-confirmation-delete-button">
+                                    <div className="modal-confirmation-title">
+                                        <p>Voulez-vous vraiment supprimer cet élève ?</p>
+                                    </div>
+                                    <div className="modal-confirmation-delete-response">
+                                        <button className="modal-confirmation-response" onClick={confirmationModal}>Non</button>
+                                        <button className="modal-confirmation-response" onClick={() => deleteStudent(item.id)}>Oui</button>
+                                    </div>
+                                </div>
+                            </Modal>
+
+                        </div>
+                    </div>
+                    <div className="identity-modal-button-password-block">
+                <button className="identity-modal-button-password" onClick={passwordModal}>Changez le mot de passe</button>
+                </div>
+                </div>
+               
+            </Modal>
+            {console.log(item)}
+        </div>
+    )
 }
