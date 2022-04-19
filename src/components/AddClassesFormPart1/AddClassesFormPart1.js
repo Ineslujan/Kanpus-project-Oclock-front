e08f8d30ec5300f4b023f9b88cf1c4228cbc0e2c
MarkSoclock <mark.smaniotto@gmail.com>
2022-04-19 09:52:05 +0200

    Pushing last updates before merge

--- src/components/AddClassesFormPart1/AddClassesFormPart1.js
+++ src/components/AddClassesFormPart1/AddClassesFormPart1.js
@@ -102,14 +102,14 @@ export default function AddClassesFormPart1({
             :
                 <form className="create-date-form" onSubmit={onSubmit}>
                     <div className="create-date-form-container">
-                        <label htmlFor="create-date-name" className="create-date-name-label">nom du cours</label>
+                        <label htmlFor="create-date-name" className="create-date-name-label">Nom du cours</label>
                         <input type="text" className="create-date-form-input" value={courseName} onChange={changeName} />
                     </div>   
-                    <label htmlFor="create-date-name" className="create-date-name-label">choisissez une date</label>
+                    <label htmlFor="create-date-name" className="create-date-name-label">Choisissez une date</label>
                     <div className="calendar-datepicker">                        
                         <PickDate startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} />                                              
                     </div>
-                    <label htmlFor="create-date-name" className="create-date-name-label">horaires</label>
+                    <label htmlFor="create-date-name" className="create-date-name-label">Horaires</label>
                     <div className="create-date-form-time-container">
                         {showStartTimePicker ? <TimePicker time={startTime} setTime={setStartTime} setShowPicker={setShowStartTimePicker} /> : <button className="date-time-button" onClick={()=> setShowStartTimePicker(true)}>{startTime}</button> }
                         {showEndTimePicker ? <TimePicker  time={endTime} setTime={setEndTime} setShowPicker={setShowEndTimePicker} /> : <button className="date-time-button"  onClick={()=> setShowEndTimePicker(true)}>{endTime}</button> }
