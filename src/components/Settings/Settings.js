f3358faeb479a177fefcb94c1043e3dd285e1841
deldon <r.deldon@gmail.com>
2022-04-22 23:50:33 +0200

    Modifying css to setting modale and fix absses button in trainerform modal

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -4,6 +4,9 @@ import SettingsUpdate from '../SettingsUpdate/SettingsUpdate';
 import { getSettings } from '../../requests/aboutSettings';
 import { AuthenticationContext } from '../../context/authenticationContext';
 import svgCircle from '../../assets/images/icones-bags-svg/bi-x-square-fill.svg';
+import Pen from '../../assets/images/pen.png';
+import { DateTime } from "luxon";
+import './settings.scss'
 
 export default function Settings({modalIsOpen, setModalIsOpen}) {
     Modal.setAppElement(document.getElementById('root'));
@@ -54,41 +57,45 @@ export default function Settings({modalIsOpen, setModalIsOpen}) {
                 </div>
              </div>
             {settingsData&&
-                <div className="settings">
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.name}</p>
+                <div className="settings-wrapper">
+                    <div className="settings-block">
+                        <div className="settings-container">
+                        {settingsData.url_image && <img className="settings-container-image" src={settingsData.url_image} alt="image de l'entreprise" />}
+                        </div>
                     </div>
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.address}</p>
-                    </div>
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.email}</p>
-                    </div>
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.phone_number}</p>
-                    </div>
-                    <div className="settings-container">
-                       {settingsData.url_image && <img src={settingsData.url_image} alt="image de l'entreprise" />}
-                    </div>
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.course_start_hour_am}</p>
-                    </div>
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.course_end_hour_am}</p>
-                    </div>
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.course_start_hour_pm}</p>
-                    </div>
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.course_end_hour_pm}</p>
-                    </div>
-                    <div className="settings-container">
-                        <p className="settings-address">{settingsData.updatedAt}</p>
-                    </div>
+                        <div className="settings-block"></div>
+                        <div className="settings-container-title">
+                            <p className="settings-address">{settingsData.name}</p>
+                        </div>
+                        <div className="settings-container">
+                            <p className="settings-address">{settingsData.address}</p>
+                        </div>
+                        <div className="settings-container">
+                            <p className="settings-address">{settingsData.email}</p>
+                        </div>
+                        <div className="settings-container">
+                            <p className="settings-address">{settingsData.phone_number}</p>
+                        </div>
+                        <div className="settings-container">
+                            <p className="settings-address">{settingsData.course_start_hour_am}</p>
+                        </div>
+                        <div className="settings-container">
+                            <p className="settings-address">{settingsData.course_end_hour_am}</p>
+                        </div>
+                        <div className="settings-container">
+                            <p className="settings-address">{settingsData.course_start_hour_pm}</p>
+                        </div>
+                        <div className="settings-container">
+                            <p className="settings-address">{settingsData.course_end_hour_pm}</p>
+                        </div>
+                        <div className="settings-container">
+                            <p className="settings-address">{DateTime.fromISO(settingsData.updatedAt).weekdayLong}</p>
+                        </div>
+                        
                 </div>
             }
                 <div className="identity-modal-footer">
-                    <button className="update" onClick={seeUpdate}>*fdghdfdfgdfsgdfgfdgdfgdfsgdfgdfsgdfgdfsg</button>
+                    <button className="identity-modal-button-edit" onClick={seeUpdate}><img className="" src={Pen} alt="close-icon" /></button>
                 </div>
                 {updateModal && <SettingsUpdate isOpen={updateModal} setIsOpen={setUpdateModal} seeUpdate={seeUpdate} data={settingsData} setUpdateScreen={setUpdateScreen} /> }
  
